7a5bb145e86a38abe115102183a1a87dfe007da8
Diogo Ribeiro <diogjp10@gmail.com>
2020-08-22 16:05:15 +0100

    wip

--- HelloJussyWoo/UI/BaseFormView.swift
+++ HelloJussyWoo/UI/BaseFormView.swift
@@ -2,51 +2,69 @@ import SwiftUI
 
 struct TaxFormView: View {
     
-      var body: some View {
-          VStack {
+    var body: some View {
+        VStack {
             BaseFormView(value: .tax)
-          }
-      }
+        }
+    }
 }
 
 struct TargetSalesFormView: View {
     
-      var body: some View {
-          VStack {
+    var body: some View {
+        VStack {
             BaseFormView(value: .targetInSales)
-          }
-      }
+        }
+    }
 }
 
 struct CountryFormView: View {
     
-      var body: some View {
-          VStack {
+    var body: some View {
+        VStack {
             BaseFormView(value: .currency)
-          }
-      }
+        }
+    }
 }
 
 
 //MARK -
 
 
- enum BaseFormType {
-     case tax
-     case targetInSales
-     case currency
- }
+enum BaseFormType {
+    case tax
+    case targetInSales
+    case currency
+}
 
 class BaseFormViewModel : FixedExpensesViewModel {
     
-    func save(type: BaseFormType, value: String) {
+    var previewOptions = ["Brazil", "Europe"]
+    
+    func getTitle(type: BaseFormType) -> String {
         switch type {
         case .tax:
-            print("save tax")
+            return "Tax"
         case .targetInSales:
-            print("save target")
+            return "Target in Sales"
         case .currency:
-            print("save currency")
+            return "Currency"
+        }
+    }
+    
+    func saveCurrency(value: Int) {
+        
+    }
+    
+    func save(type: BaseFormType, value: String) {
+         print("value: \(value)")
+        switch type {
+        case .tax:
+            print("in tax")
+        case .targetInSales:
+            print("in target")
+        case .currency:
+            print("in currency")
         }
     }
 }
@@ -55,30 +73,53 @@ struct BaseFormView: View {
     @State var value: String = ""
     @ObservedObject var viewModel: BaseFormViewModel
     private var type:BaseFormType = .tax
-
+    @State private var previewIndex = 0
+    var previewOptions: [String] = []
+    
     init(value: BaseFormType) {
         self.type = value
         self.viewModel = BaseFormViewModel()
+        self.previewOptions = self.viewModel.previewOptions
     }
     
     var body: some View {
-        NavigationView {
+        VStack {
             Form {
                 
-               Section(header: Text("PROFILE")) {
-                    TextField("Username", text: $value)
+                Section(header: Text("")) {
+                    
+                    if self.type == .currency {
+                        Picker(selection: $previewIndex, label: Text("")) {
+                            ForEach(0 ..< previewOptions.count) {
+                                Text(self.previewOptions[$0])
+                            }
+                        }.pickerStyle(WheelPickerStyle())
+                    }else {
+                        TextField("Type in your value", text: $value)
+                    }
+                    
                 }
                 
                 Section {
                     Button(action: {
-                       print("save")
-                        self.viewModel.save(type: self.type, value: self.value)
+                        
+                        if self.type == .currency {
+                            self.viewModel.saveCurrency(value: self.previewIndex)
+                        }else {
+                           self.viewModel.save(type: self.type, value: self.value)
+                        }
+                        
+                        
+                        
                     }) {
                         Text("Save changes")
                     }
                 }
             }
-        }
+            
+            
+        }.navigationBarTitle(self.viewModel.getTitle(type: self.type))
+            .listStyle(GroupedListStyle())
     }
 }
 
